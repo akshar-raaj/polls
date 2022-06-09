6375b7be7cf4219fd3ade2133d0d232caf696c0a
Akshar Raaj <akshar@urbanpiper.com>
2022-06-09 22:55:36 +0530

    Part 2: Test model Question

--- polls/tests.py
+++ polls/tests.py
@@ -1,7 +1,26 @@
+import pytest
+import datetime
+
 from django.test import Client
+from django.utils import timezone
+
+from .models import Question
 
 
 def test_polls():
     client = Client()
     response = client.get('/polls/')
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_model_question():
+    pub_date = timezone.now()
+    question = Question.objects.create(question_text="How's the weather?",
+                                       pub_date=pub_date)
+    assert question.was_published_recently() is True
+
+    pub_date = timezone.now() - datetime.timedelta(days=2)
+    question = Question.objects.create(question_text="How's the weather?",
+                                       pub_date=pub_date)
+    assert question.was_published_recently() is False

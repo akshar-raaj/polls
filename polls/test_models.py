e4ecd3aaabda0b0e24fd3045a1b329fb9a2efdd1
Akshar Raaj <akshar@urbanpiper.com>
2022-06-09 23:22:34 +0530

    Part 2: Split assertions into separate tests and group tests in a class.

--- polls/test_models.py
+++ polls/test_models.py
@@ -6,14 +6,18 @@ from django.utils import timezone
 from .models import Question
 
 
-@pytest.mark.django_db
-def test_model_question():
-    pub_date = timezone.now()
-    question = Question.objects.create(question_text="How's the weather?",
-                                       pub_date=pub_date)
-    assert question.was_published_recently() is True
+class TestModels():
 
-    pub_date = timezone.now() - datetime.timedelta(days=2)
-    question = Question.objects.create(question_text="How's the weather?",
-                                       pub_date=pub_date)
-    assert question.was_published_recently() is False
+    @pytest.mark.django_db
+    def test_question_published_recently(self):
+        pub_date = timezone.now()
+        question = Question.objects.create(question_text="How's the weather?",
+                                           pub_date=pub_date)
+        assert question.was_published_recently() is True
+
+    @pytest.mark.django_db
+    def test_question_not_published_recently(self):
+        pub_date = timezone.now() - datetime.timedelta(days=2)
+        question = Question.objects.create(question_text="How's the weather?",
+                                           pub_date=pub_date)
+        assert question.was_published_recently() is False

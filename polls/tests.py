6c248e9821670a2c35bc8027b584c87c13e1ce7d
Akshar Raaj <akshar@urbanpiper.com>
2022-06-09 21:43:59 +0530

    Part 1: Test /polls/ endpoint

--- polls/tests.py
+++ polls/tests.py
@@ -1,2 +1,7 @@
-def test_one():
-    assert 1 == 1
+from django.test import Client
+
+
+def test_polls():
+    client = Client()
+    response = client.get('/polls/')
+    assert response.status_code == 200

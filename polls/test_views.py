4652eaa1eda7f01da16e18d034e79b2d8ba1cda7
Akshar Raaj <akshar@urbanpiper.com>
2022-06-10 23:47:13 +0530

    Part 3: Use fixture 'client' provided by plugin pytest-django.

--- polls/test_views.py
+++ polls/test_views.py
@@ -1,7 +1,6 @@
 import pytest
 import datetime
 
-from django.test import Client
 from django.utils import timezone
 
 from .models import Question
@@ -10,13 +9,12 @@ from .models import Question
 class TestViews():
 
     @pytest.mark.django_db
-    def test_index(self):
-        client = Client()
+    def test_index(self, client):
         response = client.get('/polls/')
         assert response.status_code == 200
 
     @pytest.mark.django_db
-    def test_index_num_questions(self):
+    def test_index_num_questions(self, client):
         """
         This test asserts that latest n questions are shown by the index view.
         :return:
@@ -27,7 +25,6 @@ class TestViews():
             question = Question.objects.create(question_text=question_text,
                                                pub_date=pub_date)
 
-        client = Client()
         response = client.get('/polls/')
         assert b'first' not in response.content
         for question_text in question_texts[1:]:
@@ -35,10 +32,9 @@ class TestViews():
 
     @pytest.mark.django_db
     @pytest.mark.parametrize('url_fragment', ['', 'results/', 'vote/'])
-    def test_detail_views(self, url_fragment):
+    def test_detail_views(self, client, url_fragment):
         pub_date = timezone.now()
         question = Question.objects.create(question_text='How you doin? <Joey style>',
                                            pub_date=pub_date)
-        client = Client()
         response = client.get('/polls/{}/{}'.format(question.pk, url_fragment,))
         assert response.status_code == 200
